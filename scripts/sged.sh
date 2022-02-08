c17822ec2e2cc3fcbb0dd67f5e37b5f11c3d9fc7
XianYuGong <gongpeiyuan1@163.com>
2022-02-08 23:24:50 +0800

    modify bash scripts

--- scripts/sged.sh
+++ scripts/sged.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=0 nohup python spell_gec/train.py \
+CUDA_VISIBLE_DEVICES=0 nohup python ged_classification/train.py \
           --model_name_or_path bert-base-chinese \
           --train_batch_size 128 \
           --valid_batch_size 64 \
           --epoch 100 \
           --lr 3e-5 \
           --patience 3 \
-          --output_dir spell_gec/checkpoints/bert \
-          > log/spell_gec_bert.log 2>&1 &
+          --output_dir ged_classification/checkpoints/bert \
+          > log/sged_bert.log 2>&1 &
